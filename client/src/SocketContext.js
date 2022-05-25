a2451a05bc36ea2ba243ed076be2b9f39dc9ddd7
PraveenRaoVP <praveenraovp01@gmail.com>
2022-05-25 15:42:42 +0530

    deployment commit

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000')
+const socket = io('https://git.heroku.com/hangout-homie-video-chat.git')
 
 const ContextProvider = ({ children }) => {
     const [me, setMe] = useState('')
